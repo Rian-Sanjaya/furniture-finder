58ee58c72b698d7722a79b45c103152557776b14
Rian <rian.sj@gmail.com>
2019-09-09 15:02:54 +0700

    Updates

---
+++ precache-manifest.7e1c3133c321c1f277b107168c76ae3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77a7a205c86c4bfdecc9f2608f872ee",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "13f3532b1e25c397a9be",
+    "url": "/furniture-finder/static/css/main.4a6272c2.chunk.css"
+  },
+  {
+    "revision": "a418d3d57fba63956699",
+    "url": "/furniture-finder/static/js/2.db9c8126.chunk.js"
+  },
+  {
+    "revision": "13f3532b1e25c397a9be",
+    "url": "/furniture-finder/static/js/main.30944c30.chunk.js"
+  },
+  {
+    "revision": "b3314e315a3013eafd63",
+    "url": "/furniture-finder/static/js/runtime~main.e6c72ce6.js"
+  }
+]);
